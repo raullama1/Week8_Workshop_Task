f607b5be3efff84e3429879ae77567154ba75896
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-12-15 11:27:34 +0545

    Task1 completed.

---
+++ Week8/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Week8;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "Week8" };
+    }
+}
---
+++ Week8/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Week8;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
