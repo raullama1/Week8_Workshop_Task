0cb6223dc59eab761a63c8768b8b8308ab18f6bb
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-12-15 14:46:32 +0545

    Task 4.1 completed

--- Week8/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/Week8.AssemblyInfo.cs
+++ Week8/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/Week8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a8a151081593db3948e2a1a28e2045470b4ec658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+19987910faa625b71249f8bc06284c31b9c2bd0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
